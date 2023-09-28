4942037076649e5936f1a0f6e463c542bdc65599
Charlie H <charles_hopkins@me.com>
2023-09-28 11:50:38 -0500

    Made changes to navbar and updated the logo to fit more appropriately on the screen.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/logo.png';
+
+const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#possibility">Possibility</a></p>
+  <p><a href="#features">Projects</a></p>
+  <p><a href="#blog">Blog</a></p>
+  </>
+)
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className = "portfolio__navbar">
       <div className = "portfolio__navbar-links">
         <div className = "portfolio__navbar-links_logo">
-          <img src={logo} alt="logo" style={{height:30, width:118}}/>
+          <img src={logo} alt="logo"style={{height:20, width:85, alignSelf: 'left'}}/>
+        </div>
+        <div className="portfolio__navbar-links_container">
+          <Menu />
+        </div>
+        <div className="portfolio__navbar-menu">
+          {toggleMenu
+            ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/>
+            : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/>
+          }
+          {toggleMenu && (
+            <div className="portfolio__navbar-menu_container scale-up-center">
+              <div className="portfolio__navbar-menu_container-links">
+                <Menu />
+              </div>
+            </div>
+          )
+          }
         </div>
       </div>
     </div>
