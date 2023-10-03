8ae91c5cc704614436510cbcd7f73f2be860662e
Charlie H <charles_hopkins@me.com>
2023-10-03 04:36:58 -0500

    Updated header component to fix issues with certain mobile devices.

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,15 +7,15 @@ const Header = () => (
     <div className="portfolio__header section__padding" id="home">
       <div className = "portfolio__header-content">
         <h1 className="gradient__text">
-          Hi, I'm Charlie and I am a software developer/database designer.
+          Hi, I'm Charlie. I am a software developer/database designer.
         </h1>
-        <p> Back End Developer who focuses on writting clean and effcicient code. I like to try new things as well and explore the possibilities of programming. I love Java, Python and the C's with a little React JS.</p>
-
+        <p> I'm a back end developer and focus on writting clean and effcicient code. I like to try new things as well and explore the possibilities of programming. I love Java, Python and the C's with a little React JS.</p>
+        {/*
         <div className="portfolio__header-content__input">
           <input type="email" placeholder="Your email address" />
           <button type="button"> Contact Me </button>
         </div>
-
+        */}
         <div className="portfolio__header-content__people">
           <img src={people} alt="people"/>
           <p>Please feel free to network with me...</p>
